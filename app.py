1c1e627dcc4e93ece2df6fd7824f4ce9edda53db
afkarami <a.fahmi.karami@gmail.com>
2025-10-08 21:41:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,23 +64,27 @@ if nim:
                 "q6": q6
             })
 
-    #     if st.button("Kirim Semua"):
-    #         res = requests.post(SCRIPT_URL, json=survey_data)
-    #         if res.status_code == 200 and "OK" in res.text:
-    #             st.success("Data berhasil dikirim!")
-    #         else:
-    #             st.error("Gagal mengirim data.")
-    # else:
-    #     st.warning("NIM tidak ditemukan dalam database.")
+        if not all_filled:
+            st.warning("Harap isi semua pertanyaan sebelum mengirim.")
+            st.button("Kirim Semua",disabled=True)
+        else:
+            if st.button("Kirim Semua"):
+                res = requests.post(SCRIPT_URL, json=survey_data)
+                if res.status_code == 200 and "OK" in res.text:
+                    st.success("Data berhasil dikirim!")
+                else:
+                st.error("Gagal mengirim data.")
+        else:
+            st.warning("NIM tidak ditemukan dalam database.")
 
-# Submission button
-            if not all_filled:
-                st.warning("Harap isi semua pertanyaan sebelum mengirim.")
-                st.button("Kirim Semua", disabled=True)
-            else:
-                if st.button("Kirim Semua"):
-                    res = requests.post(SCRIPT_URL, json=survey_data)
-                    if res.status_code == 200 and "OK" in res.text:
-                        st.success("Data berhasil dikirim!")
-                    else:
-                        st.error("Gagal mengirim data.")
+# # Submission button
+#             if not all_filled:
+#                 st.warning("Harap isi semua pertanyaan sebelum mengirim.")
+#                 st.button("Kirim Semua", disabled=True)
+#             else:
+#                 if st.button("Kirim Semua"):
+#                     res = requests.post(SCRIPT_URL, json=survey_data)
+#                     if res.status_code == 200 and "OK" in res.text:
+#                         st.success("Data berhasil dikirim!")
+#                     else:
+#                         st.error("Gagal mengirim data.")
