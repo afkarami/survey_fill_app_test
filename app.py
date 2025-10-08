47d059f6f727e3f8b2217ddc9ddac1fc6632b5ff
afkarami <a.fahmi.karami@gmail.com>
2025-10-08 22:19:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,15 +78,3 @@ if nim:
                     st.error("Gagal mengirim data.")
             else:
                 st.warning("NIM tidak ditemukan dalam database.")
-
-# # Submission button
-#             if not all_filled:
-#                 st.warning("Harap isi semua pertanyaan sebelum mengirim.")
-#                 st.button("Kirim Semua", disabled=True)
-#             else:
-#                 if st.button("Kirim Semua"):
-#                     res = requests.post(SCRIPT_URL, json=survey_data)
-#                     if res.status_code == 200 and "OK" in res.text:
-#                         st.success("Data berhasil dikirim!")
-#                     else:
-#                         st.error("Gagal mengirim data.")
