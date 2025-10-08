8869eee28c0d56500f72d67dfdfbe841728ba325
afkarami <a.fahmi.karami@gmail.com>
2025-10-08 22:27:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,17 +40,6 @@ if nim:
             q5 = st.selectbox("5. Dosen memberikan umpan balik yang membangun terhadap hasil kerja mahasiswa",options=[1, 2, 3, 4, 5],index=0,key=f"{course}_q5")
             q6 = st.selectbox("6. Secara umum, proses pembelajaran pada mata kuliah ini berjalan dengan baik",options=[1, 2, 3, 4, 5],index=0,key=f"{course}_q6")
 
-            # cols = st.columns(6)
-            
-
-            # Start with blank values (None)
-            # q1 = cols[0].number_input("1. 1. Dosen menguasai materi pembelajaran dengan baik dan mudah dipahami, menggunakan sumber-sumber referensi terbaru serta mengembangkan gagasan baru/inovatif", 1, 5, key=f"{course}_q1", value=None, placeholder="Isi 1–5")
-            # q2 = cols[1].number_input("Q2", 1, 5, key=f"{course}_q2", value=None, placeholder="Isi 1–5")
-            # q3 = cols[2].number_input("Q3", 1, 5, key=f"{course}_q3", value=None, placeholder="Isi 1–5")
-            # q4 = cols[3].number_input("Q4", 1, 5, key=f"{course}_q4", value=None, placeholder="Isi 1–5")
-            # q5 = cols[4].number_input("Q5", 1, 5, key=f"{course}_q5", value=None, placeholder="Isi 1–5")
-            # q6 = cols[5].number_input("Q6", 1, 5, key=f"{course}_q6", value=None, placeholder="Isi 1–5")
-
             # Check if any question is left blank
     if None in (q1, q2, q3, q4, q5, q6):
         all_filled = False
@@ -66,15 +55,15 @@ if nim:
                 "q6": q6
             })
         
-        if not all_filled:
-            st.warning("Harap isi semua pertanyaan sebelum mengirim.")
-            st.button("Kirim Semua",disabled=True)
-        else:
-            if st.button("Kirim Semua"):
-                res = requests.post(SCRIPT_URL, json=survey_data)
-                if res.status_code == 200 and "OK" in res.text:
-                    st.success("Data berhasil dikirim!")
-                else:
-                    st.error("Gagal mengirim data.")
+    if not all_filled:
+        st.warning("Harap isi semua pertanyaan sebelum mengirim.")
+        st.button("Kirim Semua",disabled=True)
+    else:
+        if st.button("Kirim Semua"):
+            res = requests.post(SCRIPT_URL, json=survey_data)
+            if res.status_code == 200 and "OK" in res.text:
+                st.success("Data berhasil dikirim!")
             else:
-                st.warning("NIM tidak ditemukan dalam database.")
+                st.error("Gagal mengirim data.")
+        else:
+            st.warning("NIM tidak ditemukan dalam database.")
